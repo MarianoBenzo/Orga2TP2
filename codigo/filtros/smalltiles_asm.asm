29e6696a98bc62b1120919ad1ad8d8cb13ad5b93
franketi01 <francogm01@gmail.com>
2016-09-13 20:36:29 -0300

    Add files via upload

--- codigo/filtros/smalltiles_asm.asm
+++ codigo/filtros/smalltiles_asm.asm
@@ -1,8 +1,59 @@
+
 section .data
 DEFAULT REL
 
 section .text
 global smalltiles_asm
 smalltiles_asm:
+
 ;COMPLETAR
+	;RDI = src , RSI = dst , EDX = cols , ECX = filas
+	;R8D = src_row_size, R9D = dst_row_size
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	
+	mov r12, rsi								;guardo la dir del dst en R12
+	shr edx, 1									; RDX = RDX/2
+	shr ecx, 1									; RCX = RCX/2
+	xor rax, rax
+	xor rbx, rbx
+	xor r13, r13
+	xor r14, r14
+	
+	mov r13d, edx              					;r13d = cols/2 
+	mov r14d, ecx								;r14d = filas/2
+	mov eax, r13d
+	mov ebx, r14d
+	mov r15d, 0
+	mul r14
+	mov ecx, eax 
+	.ciclo:
+		movdqu xmm1, [rdi]
+		;movdqu xmm2, [rdi + 16]
+		;pshuf
+		movdqu [rsi], xmm1
+		movdqu [rsi + 8*rax], xmm1
+		add rdi, 16
+		add rsi, 8
+		inc r15d
+		cmp r15d,r13d
+		jl .cont
+		add r12, r9
+		mov rsi, r12
+		mov r15d, 0
+	.cont:	
+		loop .ciclo
+	
+		
+	pop r15	
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
 	ret
--- codigo/filtros/smalltiles_c.c
+++ codigo/filtros/smalltiles_c.c
@@ -7,7 +7,7 @@ void smalltiles_c (unsigned char *src, unsigned char *dst, int cols, int filas, 
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 	
 	// ejemplo de uso de src_matrix y dst_matrix (copia la imagen)
-
+/*
 	for (int f = 0; f < filas; f++) {
 		for (int c = 0; c < cols; c++) {
 			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
@@ -17,6 +17,41 @@ void smalltiles_c (unsigned char *src, unsigned char *dst, int cols, int filas, 
 			p_d->g = p_s->g;
 			p_d->r = p_s->r;
 			p_d->a = p_s->a;
+
+		}
+	}*/
+	
+	for(int f = 0; f < filas/2; f++)
+	{
+		for(int c = 0; c < cols/2; c++)
+		{
+			bgra_t* p_sa = (bgra_t*) &src_matrix[2*f][2*(c*4)];
+			//bgra_t* p_sb = (bgra_t*) &src_matrix[f + 4][c*4];
+			
+			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c*4];
+			bgra_t *p_e = (bgra_t*) &dst_matrix[f + filas/2][c*4];
+			bgra_t *p_f = (bgra_t*) &dst_matrix[f][(c + cols/2)*4];
+			bgra_t *p_g = (bgra_t*) &dst_matrix[f + filas/2 ][(c + cols/2)*4];
+
+			p_d->b = p_sa->b;
+			p_d->g = p_sa->g;
+			p_d->r = p_sa->r;
+			p_d->a = p_sa->a;
+			
+			p_e->b = p_sa->b;
+			p_e->g = p_sa->g;
+			p_e->r = p_sa->r;
+			p_e->a = p_sa->a;
+			
+			p_f->b = p_sa->b;
+			p_f->g = p_sa->g;
+			p_f->r = p_sa->r;
+			p_f->a = p_sa->a;
+			
+			p_g->b = p_sa->b;
+			p_g->g = p_sa->g;
+			p_g->r = p_sa->r;
+			p_g->a = p_sa->a;
 
 		}
 	}
