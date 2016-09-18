40c60a63ba94630018e884b44d5ab31f20f35db0
FerminT <FerminT@users.noreply.github.com>
2016-09-18 13:46:51 -0300

    Colorizar_asm sin minimo
    Solo falta la parte de multiplicar cada componente por su correspondiente phi y buscar el mínimo entre 255 y eso.

--- codigo/filtros/colorizar_asm.asm
+++ codigo/filtros/colorizar_asm.asm
@@ -20,6 +20,9 @@
 section .rodata
 intercambiar: DB 0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0A, 0x0B, 0x04, 0x05, 0x06, 0x07, 0x0C, 0x0D, 0x0E, 0x0F
 acercar: DB 0x00, 0x01, 0x02, 0x03, 0x0C, 0x0D, 0x0E, 0x0F, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B
+negar: DW 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF
+unosW: DW 1, 1, 1, 1, 1, 1, 1, 1
+unosF: DD 1.0, 1.0, 1.0, 1.0
 
 global colorizar_asm
 
@@ -30,6 +33,8 @@ colorizar_asm:
 	mov rbp, rsp
 	push rbx
 	push r12
+	push r13
+	push r14
 
 	xor r8, r8
 	mov r8d, edx
@@ -54,6 +59,9 @@ colorizar_asm:
 	movups xmm3, xmm0			; xmm3 = | 0 | 0 | alpha | alpha |
 	pslldq xmm3, 8				; xmm3 = | alpha | alpha | 0 | 0 |
 	addps xmm0, xmm3 			; xmm0 = | alpha | alpha | alpha | alpha |
+	movdqu xmm8, [negar]		; xmm8 = | 0xFFFF | 0xFFFF | 0xFFFF | 0xFFFF | 0xFFFF | 0xFFFF | 0xFFFF | 0xFFFF |
+	movdqu xmm9, [unosW]		; xmm9 = | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 
+	movdqu xmm10, [unosF]		; xmm10= | 1.0 | 1.0 | 1.0 | 1.0 |
 	.ciclo:
 		cmp ecx, 0	 				; si no hay mas filas por recorrer, termine
 		je .fin
@@ -78,14 +86,57 @@ colorizar_asm:
 		movdqu xmm3, xmm1			; xmm3 = | maxA_2 | maxR_2 | maxG_2 | maxB_2 | maxA_1 | maxR_1 | maxG_1 | maxB_1 | 
 		; 132 = 10 00 01 00
 		; copio al maxR para usarlo en el shuffle de xmm3, pero esa columna no me importa
-		pshuflw xmm1, xmm3, 132		; xmm1 = | maxA_2 | maxR_2 | maxG_2 | maxB_2 | maxA_1 | maxB_1 | maxG_1 | maxB_1 |
+		pshuflw xmm1, xmm3, 132		; xmm1 = | maxA_2 | maxR_2 | maxG_2 | maxB_2 | maxR_1 | maxB_1 | maxG_1 | maxB_1 |
+		; copio a xmm1, porque el shuffle high me reemplaza la parte low
+		movdqu xmm3, xmm1			; xmm3 = | maxA_2 | maxR_2 | maxG_2 | maxB_2 | maxR_1 | maxB_1 | maxG_1 | maxB_1 |
 		pshufhw xmm1, xmm3, 132		; xmm1 = | maxR_2 | maxB_2 | maxG_2 | maxB_2 | maxR_1 | maxB_1 | maxG_1 | maxB_1 |
 		; 31 = 00 01 11 11
-		; copio al maxB para no perder datos, pero esa columna no me importa
-		pshuflw xmm3, xmm1, 31		; xmm3 = | maxA_2 | maxR_2 | maxG_2 | maxB_2 | maxB_1 | maxG_1 | maxR_1 | maxR_1 |
-		pshufhw xmm3, xmm1, 31		; xmm3 = | maxB_2 | maxG_2 | maxR_2 | maxR_2 | maxB_1 | maxG_1 | maxR_1 | maxR_1 |
+		; copio al maxB, pero esa columna no me importa
+		pshuflw xmm3, xmm1, 31		; xmm3 = | maxR_2 | maxR_2 | maxG_2 | maxB_2 | maxB_1 | maxG_1 | maxR_1 | maxR_1 |
+		; uso a xmm4 de auxiliar para no perder la parte baja en el shuffle
+		movdqu xmm4, xmm3			; xmm4 = | maxR_2 | maxR_2 | maxG_2 | maxB_2 | maxB_1 | maxG_1 | maxR_1 | maxR_1 |
+		pshufhw xmm3, xmm4, 31		; xmm3 = | maxB_2 | maxG_2 | maxR_2 | maxR_2 | maxB_1 | maxG_1 | maxR_1 | maxR_1 |
 		; ahora tengo los datos alineados según las comparaciones que quiero hacer
 		pcmpgtw xmm1, xmm3			; xmm1 = | ? | (maxB_2 > maxG_2)? | (maxG_2 > maxR_2)? | (maxB_2 > maxR_2)? | ? | (maxB_1 > maxG_1)? | (maxG_1 > maxR_1)? | (maxB_1 > maxR_1)? |
+		movdqu xmm3, xmm1			; xmm3 = | ? | (maxB_2 > maxG_2)? | (maxG_2 > maxR_2)? | (maxB_2 > maxR_2)? | ? | (maxB_1 > maxG_1)? | (maxG_1 > maxR_1)? | (maxB_1 > maxR_1)? |
+		; obtengo el complemento de las comparaciones
+		pxor xmm3, xmm8				; xmm3 = | ? | (maxG_2 >= maxB_2)? | (maxR_2 >= maxG_2)? | (maxR_2 >= maxB_2)? | ? | (maxG_1 >= maxB_1)? | (maxR_1 >= maxG_1)? | (maxR_1 >= maxB_1)? |
+		; extraigo (maxB > maxR)? para insertarlo en xmm3
+		pextrw r13d, xmm1, 0 		; r13w = (maxB_1 > maxR_1)?
+		pextrw r14d, xmm1, 4		; r14w = (maxB_2 > maxR_2)?
+		pinsrw xmm3, r13d, 3 		; xmm3 = | ? | (maxG_2 >= maxB_2)? | (maxR_2 >= maxG_2)? | (maxR_2 >= maxB_2)? | (maxB_1 > maxR_1)? | (maxG_1 >= maxB_1)? | (maxR_1 >= maxG_1)? | (maxR_1 >= maxB_1)? |
+		pinsrw xmm3, r14d, 7		; xmm3 = | (maxB_2 > maxR_2)? | (maxG_2 >= maxB_2)? | (maxR_2 >= maxG_2)? | (maxR_2 >= maxB_2)? | (maxB_1 > maxR_1)? | (maxG_1 >= maxB_1)? | (maxR_1 >= maxG_1)? | (maxR_1 >= maxB_1)? |
+		; extraigo (maxR >= maxB)? para insertarlo en xmm1
+		pextrw r13d, xmm3, 0 		; r13w = (maxR_1 >= maxB_1)?
+		pextrw r14d, xmm3, 4		; r14w = (maxR_2 >= maxB_2)?
+		pinsrw xmm1, r13d, 0   		; xmm1 = | ? | (maxB_2 > maxG_2)? | (maxG_2 > maxR_2)?  | (maxB_2 > maxR_2)?  | ? | (maxB_1 > maxG_1)? | (maxG_1 > maxR_1)?  | (maxR_1 >= maxB_1)? |
+		pinsrw xmm1, r14d, 4 		; xmm1 = | ? | (maxB_2 > maxG_2)? | (maxG_2 > maxR_2)?  | (maxR_2 >= maxB_2)? | ? | (maxB_1 > maxG_1)? | (maxG_1 > maxR_1)?  | (maxR_1 >= maxB_1)? |
+		; acomodo los datos
+		psrldq xmm3, 2   			; xmm3 = | ? | (maxB_2 > maxR_2)? | (maxG_2 >= maxB_2)? | (maxR_2 >= maxG_2)? | ? | (maxB_1 > maxR_1)? | (maxG_1 >= maxB_1)? | (maxR_1 >= maxG_1)? |
+		pand xmm1, xmm3				; xmm1 = | ? | condicion(phiB_2) | condicion(phiG_2) | condicion(phiR_2) | ? | condicion(phiB_1) | condicion(phiG_1) | condicion(phiR_1) |
+		movdqu xmm7, xmm1			; xmm7 = | ? | condicion(phiB_2) | condicion(phiG_2) | condicion(phiR_2) | ? | condicion(phiB_1) | condicion(phiG_1) | condicion(phiR_1) |
+		movdqu xmm3, xmm1			; xmm3 = | ? | condicion(phiB_2) | condicion(phiG_2) | condicion(phiR_2) | ? | condicion(phiB_1) | condicion(phiG_1) | condicion(phiR_1) |
+		; genero la mascara con el valor uno donde la condicion dio true
+		pand xmm7, xmm9
+		; niego las condiciones originales y sumo
+		pxor xmm3, xmm8		
+		paddw xmm3, xmm7			; xmm3 = 0x0001 donde dio true y 0xFFFF donde dio false
+		; ahora resta extender a dword, convertirlo a float y multiplicarlo por alpha
+		pxor xmm4, xmm4
+		pcmpgtw xmm4, xmm3
+		movdqu xmm1, xmm3			; xmm1 = 0x0001 donde dio true y 0xFFFF donde dio false
+		punpcklwd xmm1, xmm4		; xmm1 = 0x0001 donde dio true y 0xFFFF donde dio false (PX1)
+		punpckhwd xmm3, xmm4		; xmm3 = 0x0001 donde dio true y 0xFFFF donde dio false (PX2)
+		cvtdq2ps xmm1, xmm1
+		cvtdq2ps xmm3, xmm3
+		mulps xmm1, xmm8			; xmm1 = alpha donde dio true y -alpha donde dio false (PX1)
+		mulps xmm3, xmm8			; xmm3 = alpha donde dio true y -alpha donde dio false (PX2)
+		movdqu xmm4, xmm10			; xmm4 = | 1.0 | 1.0 | 1.0 | 1.0 |
+		movdqu xmm5, xmm10 			; xmm5 = | 1.0 | 1.0 | 1.0 | 1.0 |
+		addps xmm4, xmm1 			; xmm4 = | ? | phiB_1 | phiG_1 | phiR_1 |
+		addps xmm5, xmm3 			; xmm5 = | ? | phiB_2 | phiG_2 | phiR_2 |
+		; en xmm2 tengo los datos de los pixeles, extender cada uno a dword (primero shiftear) y hacer las cuentas
+
 
 		cmp edx, 0
 		jne .seguir
@@ -103,4 +154,3 @@ colorizar_asm:
 			jmp .ciclo
 
 	ret
-
