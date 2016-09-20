ee35c24869c244249f8b6b3dd6eb6be1d116f513
franketi01 <francogm01@gmail.com>
2016-09-20 17:00:25 -0300

    Add files via upload

--- codigo/filtros/smalltiles_asm.asm
+++ codigo/filtros/smalltiles_asm.asm
@@ -9,48 +9,64 @@ smalltiles_asm:
 ;COMPLETAR
 	;RDI = src , RSI = dst , EDX = cols , ECX = filas
 	;R8D = src_row_size, R9D = dst_row_size
+	
 	push rbp
 	mov rbp, rsp
 	push rbx
 	push r12
 	push r13
 	push r14
-	push r15
-	
-	mov r12, rsi								;guardo la dir del dst en R12
-	shr edx, 1									; RDX = RDX/2
-	shr ecx, 1									; RCX = RCX/2
+
 	xor rax, rax
-	xor rbx, rbx
-	xor r13, r13
-	xor r14, r14
+	mov rbx, rsi
+	mov r14, rdi
 	
-	mov r13d, edx              					;r13d = cols/2 
-	mov r14d, ecx								;r14d = filas/2
+	shr rdx, 1
+	shr rcx, 1
+	
+	mov r12d, edx 						; r12 = cols
+	mov r13d, ecx						; r13 = filas
+	
+	shr r12, 1
+	shr r13, 1
+	
 	mov eax, r13d
-	mov ebx, r14d
+	mul r12d
+	mov rcx, rax
+	shl rax, 2
+	mov r11, rax
+	add r11, r12
 	mov r15d, 0
-	mul r14
-	mov ecx, eax 
+	mov r10, r12
+	shr r12d, 1
+
 	.ciclo:
-		movdqu xmm1, [rdi]
-		;movdqu xmm2, [rdi + 16]
-		;pshuf
-		movdqu [rsi], xmm1
-		movdqu [rsi + 8*rax], xmm1
-		add rdi, 16
-		add rsi, 8
+		movups xmm1, [rdi]								;xmm1 = |  p3  |  p2  |  p1  |  p0  |
+		movups xmm2, [rdi + 16]							;xmm2 = |  p7  |  p6  |  p5  |  p4  |
+		shufps xmm1, xmm2, 0xdd							;xmm1 = |  p6  |  p4  |  p2  |  p0  | 
+
+		movups [rsi], xmm1
+		movups [rsi + 8*rax], xmm1
+		movups [rsi + 8*r10], xmm1
+		movups [rsi + 8*r11], xmm1
+		
 		inc r15d
-		cmp r15d,r13d
-		jl .cont
-		add r12, r9
-		mov rsi, r12
+		cmp r15d, r12d
+		jne .seguir
+		lea rsi, [rbx + r9]
+		mov rbx, rsi
+		lea rdi, [r14 + 2*r8]
+		mov r14, rdi
 		mov r15d, 0
+		jmp .cont
+		.seguir:
+		add rdi, 32
+		add rsi, 16
 	.cont:	
 		loop .ciclo
 	
 		
-	pop r15	
+	;pop r15	
 	pop r14
 	pop r13
 	pop r12
